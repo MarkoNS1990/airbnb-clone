59b031fae3a4ea398f3bcd76180e80098a9f136c
MarkoNS1990 <marczecevic@gmail.com>
2021-08-11 09:49:49 +0200

    Finish banner component

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ export default function Header() {
       </div>
       {/* right */}
       <div className="flex space-x-4 items-center justify-end text-gray-500">
-        <p>Become a host</p>
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
         <GlobeAltIcon className="h-6" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
           <MenuIcon className="h-6" />
